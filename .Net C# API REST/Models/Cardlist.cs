4c690d14b0e3550979014ce0c4e3dfccda995e0e
tomastraini <93160827+tomastraini@users.noreply.github.com>
2021-12-15 23:16:42 -0300

    Add files via upload

---
+++ .Net C# API REST/Models/Cardlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_REST.Models
+{
+    public class Cardlist
+    {
+        [Key]
+        public int id_tarjeta { get; set; }
+
+        [Required]
+        public string fechaexp { get; set; }
+
+        [Required]
+        public string cvv { get; set; }
+
+    }
+}
---
+++ .Net C# API REST/Models/CardlistDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Api_REST.Models
+{
+    public class CardlistDBContext: DbContext
+    {
+        public CardlistDBContext(DbContextOptions<CardlistDBContext> options): base(options)
+        {
+
+        }
+        public DbSet<Cardlist> Cardlists { get; set; }
+    }
+}
